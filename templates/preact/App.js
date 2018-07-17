6e98c82337af17ac8712c11e4ada4fa2ab321a3f
Geoffrey Goodman <ggoodman@gmail.com>
2018-07-17 09:45:37 -0400

    Wrap preact starter in div

--- templates/preact/App.js
+++ templates/preact/App.js
@@ -5,10 +5,10 @@ import { h, Component } from 'preact';
 export default class App extends Component {
     render() {
         return (
-            <>
+            <div>
                 <h1>Hello Preact</h1>
                 <p>Start editing and see your changes reflected immediately!</p>
-            </>
+            </div>
         );
     }
 }
